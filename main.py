e829eff27479bcf70493ebc53e7200fa943c8cb8
Kavyanjali Karan <karankavyanjali77@gmail.com>
2026-03-31 22:25:49 +0530

    final working version

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = FastAPI()
 class Request(BaseModel):
     text: str
 
+@app.get("/")
+def home():
+    return {"message": "AI Agent Running 🚀"}
+
 @app.post("/summarize")
 def summarize(req: Request):
     result = summarize_text(req.text)
